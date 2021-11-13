d4071442f1e525a06ae3ab85b480f01255a5c7c3
ImJimmyXD <andreisimion99@yahoo.com>
2021-11-13 22:35:47 +0200

    L05

---
+++ Simion Andrei/L05/RaportEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace L05
+{
+    using Microsoft.WindowsAzure.Storage.Table;
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="RaportEntity" />.
+    /// </summary>
+    public class RaportEntity : TableEntity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RaportEntity"/> class.
+        /// </summary>
+        /// <param name="university">The university<see cref="string"/>.</param>
+        /// <param name="count">The count<see cref="int"/>.</param>
+        public RaportEntity(string university, int count)
+        {
+            this.PartitionKey = university;
+            this.RowKey = DateTime.Now.ToString("HH:mm:ss");
+            this.Count = count;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RaportEntity"/> class.
+        /// </summary>
+        public RaportEntity()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the Count.
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
---
+++ Simion Andrei/L05/Student.cs
@@ -0,0 +1,58 @@
+﻿namespace L05
+{
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    /// <summary>
+    /// Defines the <see cref="Student" />.
+    /// </summary>
+    public class Student : TableEntity
+    {
+        /// <summary>
+        /// Gets or sets the FirstName.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the LastName.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Year.
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PhoneNumber.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Faculty.
+        /// </summary>
+        public string Faculty { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Student"/> class.
+        /// </summary>
+        /// <param name="university">The university<see cref="string"/>.</param>
+        /// <param name="cnp">The cnp<see cref="string"/>.</param>
+        public Student(string university, string cnp)
+        {
+            this.PartitionKey = university;
+            this.RowKey = cnp;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Student"/> class.
+        /// </summary>
+        public Student()
+        {
+        }
+    }
+}
